499c05c597ea785aadd12d6d6d6d7ac72b6dcc8f
Shahin Zangenehpour <98chimp@users.noreply.github.com>
2015-03-17 15:15:18 -0700

    Initial Commit

---
+++ SimonSays/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimonSays
+//
+//  Created by Shahin on 2015-03-17.
+//  Copyright (c) 2015 98% Chimp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
